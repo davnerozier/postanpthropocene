356133c81783aa7753b43c202e8377133b60f657
davnerozier <davne.rozier@gmail.com>
2021-02-19 12:27:54 +0000

    reformatting

--- myScript.js
+++ myScript.js
@@ -1,52 +1,14 @@
 
 function yellowSatellites() {
-  document.getElementById("whiteSatellite1").id = "yellowSatellite1"
-  document.getElementById("whiteSatellite2").id = "yellowSatellite2"
-  document.getElementById("whiteSatellite3").id = "yellowSatellite3"
-  document.getElementById("whiteSatellite4").id = "yellowSatellite4"
-  document.getElementById("whiteSatellite5").id = "yellowSatellite5"
-
 }
 
 function whiteSatellites() {
-  document.getElementById("yellowSatellite1").id = "whiteSatellite1"
-  document.getElementById("yellowSatellite2").id = "whiteSatellite2"
-  document.getElementById("yellowSatellite3").id = "whiteSatellite3"
-  document.getElementById("yellowSatellite4").id = "whiteSatellite4"
-  document.getElementById("yellowSatellite5").id = "whiteSatellite5"
-
 }
 
-function yellowMicroNodes() {
-  document.getElementById("whiteMicroNode1").id = "yellowMicroNode1"
-  document.getElementById("whiteMicroNode2").id = "yellowMicroNode2"
-  document.getElementById("whiteMicroNode3").id = "yellowMicroNode3"
-  document.getElementById("whiteMicroNode4").id = "yellowMicroNode4"
-  document.getElementById("whiteMicroNode5").id = "yellowMicroNode5"
-  document.getElementById("whiteMicroNode6").id = "yellowMicroNode6"
-  document.getElementById("whiteMicroNode7").id = "yellowMicroNode7"
-
+function microNodesBack() {
+  document.getElementById("micro_node_front").id = "micro_node_back"
 }
 
-function whiteMicroNodes() {
-  document.getElementById("yellowMicroNode1").id = "whiteMicroNode1"
-  document.getElementById("yellowMicroNode2").id = "whiteMicroNode2"
-  document.getElementById("yellowMicroNode3").id = "whiteMicroNode3"
-  document.getElementById("yellowMicroNode4").id = "whiteMicroNode4"
-  document.getElementById("yellowMicroNode5").id = "whiteMicroNode5"
-  document.getElementById("yellowMicroNode6").id = "whiteMicroNode6"
-  document.getElementById("yellowMicroNode7").id = "whiteMicroNode7"
+function SuperNodes() {
 
-}
-
-function yellowSuperNodes() {
-  document.getElementById("whiteSuperNode1").id = "yellowSuperNode1"
-  document.getElementById("whiteSuperNode2").id = "yellowSuperNode2"
-
-
-}
-
-function whiteSuperNodes() {
-  document.getElementById("yellowSuperNode1").id = "whiteSuperNode1"
-  document.getElementById("yellowSuperNode2").id = "whiteSuperNode2"
 }
